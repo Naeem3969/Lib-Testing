89656a452ab6974c5d4b868c77d1d9dd8032dc20
Garrisonian <4343969@gmail.com>
2018-10-14 17:19:37 +0500

    LibTest

---
+++ app/src/main/java/com/home/statlibtest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.home.statlibtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import com.home.statlib.CentralTendency;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        text=(TextView)findViewById(R.id.text);
+
+
+        ArrayList<Integer> numbers = new ArrayList<>();
+        Random r = new Random();
+
+        for (int i = 0; i < 15; i++) {
+            numbers.add(r.nextInt(7)+1);
+        }
+
+        double mean = CentralTendency.arithmeticMean(numbers).doubleValue();
+        double median = CentralTendency.median(numbers);
+        ArrayList<Integer> mode = CentralTendency.mode(numbers);
+
+        Collections.sort(numbers);
+
+        String res = String.format("Numbers:\n\n%s\nMean: %.1f\nMedian: %.1f\nMode: %s\n",
+                numbers, mean, median, mode);
+
+        text.setText(res);
+    }
+}
